33244d92320618970744dd9ac2b8fcdf8bf925a0
Wagner Caixeta <wagner.caixeta@gmail.com>
2014-09-04 08:14:28 -0300

    Create mysql_split_dumpall.sh

---
+++ mysql_split_dumpall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE=$1
+if [ "$1"X == "X" ]
+then
+        echo Usage: $0 file_generated_by_dump_all.sql
+        exit
+fi
+for BD in $(cat $FILE | grep "^.. Current Database: \`" | cut -d\` -f2)
+do
+        OUT=$(echo $FILE | sed s/\.sql$/_$BD\.sql/g)
+        echo Creating: $OUT
+        sed -n "/^-- Current Database: \`$BD\`/,/^-- Current Database:/p" $FILE > $OUT
+done
